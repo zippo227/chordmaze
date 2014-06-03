e9f78b5c08b28f1254f7d0a07aab9ad1572b49c7
Katie Powell <katiepowellgames@gmail.com>
2014-06-03 13:23:51 -0700

    Cleaning up GuitarManager script

--- Assets/GuitarManager.cs
+++ Assets/GuitarManager.cs
@@ -8,17 +8,17 @@ public class GuitarManager : MonoBehaviour {
 	/* GuitarManager stores the current chord progression as a List of chordnames(enum)
 	 * and stores a dictionary to convert from enum to chord(class)*/
 
-	public List<MazemanagerScript.Chords> chordProgression;
+	public List<Chords> chordProgression;
 
-	Dictionary<MazemanagerScript.Chords, ChordObject> chordDictionary = new Dictionary<MazemanagerScript.Chords, ChordObject> ();
+	Dictionary<Chords, ChordObject> chordDictionary = new Dictionary<Chords, ChordObject> ();
 
 
 }
 
 public class ChordObject 
 {
-	private int[] stringSettings = new int[6] {};
-	private bool[] playStringSettings = new bool[6] {};
+	private int[] stringSettings = new int[6];
+	private bool[] playStringSettings = new bool[6];
 
 	public ChordObject(int s0, bool b0, int s1, bool b1, int s2, bool b2, int s3, bool b3, int s4, bool b4, int s5, bool b5)
 	{
